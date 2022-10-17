708c6804902ee59f2816dcabf8a2ab98a45790b1
HenriqueFelixDev <henriquefelixdev@gmail.com>
2022-10-17 11:37:50 -0300

    feat: adiciona o serviço de arquivos estáticos

--- src/setup.js
+++ src/setup.js
@@ -1,6 +1,7 @@
 import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors'
+import serve from 'koa-static'
 
 import { router } from './routes.js'
 import { errorMiddleware } from './middlewares/index.js'
@@ -8,6 +9,7 @@ import { errorMiddleware } from './middlewares/index.js'
 const app = new Koa()
 
 // Middlewares
+app.use(serve('./public'))
 app.use(bodyParser())
 app.use(cors())
 app.use(errorMiddleware)
